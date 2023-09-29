bb588baaad815ad872b02e4e484fcdf7b6007d7e
yutak23 <yutapilafu@gmail.com>
2023-09-29 14:29:30 +0900

    feat: support for default export and add test

--- index.js
+++ index.js
@@ -6,16 +6,18 @@ import jestToSatisfySchemaInApiSpec from 'jest-openapi/dist/matchers/toSatisfySc
 const { makeApiSpec } = openapiValidator;
 
 export default (filepathOrObject, schemaName) => {
+	const openApiSpec = makeApiSpec(filepathOrObject);
+
 	expect.extend({
 		// eslint-disable-next-line no-unused-vars
-		toSatisfyApiSpec: (received, expected) => {
-			const openApiSpec = makeApiSpec(filepathOrObject);
-			return jestToSatisfyApiSpec.default(received, openApiSpec);
-		},
+		toSatisfyApiSpec: (received, expected) =>
+			typeof jestToSatisfyApiSpec === 'function'
+				? jestToSatisfyApiSpec(received, openApiSpec)
+				: jestToSatisfyApiSpec.default(received, openApiSpec),
 		// eslint-disable-next-line no-unused-vars
-		toSatisfySchemaInApiSpec: (received, expected) => {
-			const openApiSpec = makeApiSpec(filepathOrObject);
-			return jestToSatisfySchemaInApiSpec.default(received, schemaName, openApiSpec);
-		}
+		toSatisfySchemaInApiSpec: (received, expected) =>
+			typeof jestToSatisfySchemaInApiSpec === 'function'
+				? jestToSatisfySchemaInApiSpec(received, schemaName, openApiSpec)
+				: jestToSatisfySchemaInApiSpec.default(received, schemaName, openApiSpec)
 	});
 };
---
+++ tests/index.test.js
@@ -0,0 +1,24 @@
+import { describe, it, expect, beforeAll } from 'vitest';
+import appRootPath from 'app-root-path';
+
+import vitestOpenAPI from '../index.js';
+
+describe('Recreate bug (issue #XX)', () => {
+	beforeAll(() => {
+		const pathToApiSpec = appRootPath.resolve('/tests/exampleOpenApiFiles/valid/openapi3.yml');
+		vitestOpenAPI(pathToApiSpec);
+	});
+
+	const res = {
+		status: 200,
+		req: {
+			method: 'GET',
+			path: '/responseBody/string'
+		},
+		body: 'foo'
+	};
+
+	it('passes', () => {
+		expect(res).toSatisfyApiSpec();
+	});
+});
